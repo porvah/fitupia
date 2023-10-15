e1f6e1dade45393fdc0d3973b5c31756e63057ea
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-10-15 12:37:20 +0300

    Create a dot indicator when changing the welcome screen.

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../size_config/size_config.dart';
-
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/home_screen';
 
--- lib/presentation/screens/welcome_screen.dart
+++ lib/presentation/screens/welcome_screen.dart
@@ -1,42 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../widgets/welcome_page_view.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   static const String routeName = '/welcome_screen';
 
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
   final List<String> _imagePages = const [
     'assets/images/healthy_lifestyle.png',
     'assets/images/working_out.png',
     'assets/images/activity_tracker.png',
   ];
+
   final List<String> _titlePages = const [
     'Welcome to Fitupia',
     'Your personal fitness Companion',
     'Nutrition made easy',
   ];
+
   final List<String> _descriptionPages = const [
     'Personalized fitness and nutrition guidance',
     'Recommending food and exercises based on your profile. Create a daily fitness routine',
     'Track your food intake and get personalized meal plans',
   ];
 
-  const WelcomeScreen({super.key});
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: PageView.builder(
-            itemCount: _imagePages.length,
-            itemBuilder: (_, index) {
-              return WelcomePageView(
-                imagePath: _imagePages[index],
-                title: _titlePages[index],
-                description: _descriptionPages[index],
-              );
-            }),
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: _imagePages.length,
+                itemBuilder: (_, index) {
+                  return WelcomePageView(
+                    imagePath: _imagePages[index],
+                    title: _titlePages[index],
+                    description: _descriptionPages[index],
+                  );
+                },
+              ),
+            ),
+            SmoothPageIndicator(
+              controller: _pageController,
+              count: _imagePages.length,
+              effect: const ExpandingDotsEffect(
+                activeDotColor: Color(0xFFc1121f),
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
