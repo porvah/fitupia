f63a67b867b3213cdffa01b5c3ae9d45c326b0d0
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-10-22 13:48:02 +0300

    Craete a basic ui for the registration Screen.

--- lib/presentation/screens/registration_screen.dart
+++ lib/presentation/screens/registration_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_button.dart';
 import '../widgets/custom_input_field.dart';
 
 class RegistrationScreen extends StatelessWidget {
@@ -13,10 +14,39 @@ class RegistrationScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Registration'),
       ),
-      body: const SafeArea(
-        child: CustomInputField(
-          hintText: 'Name',
-          icon: Icon(Icons.person),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const CustomInputField(
+              hintText: 'Name',
+              icon: Icon(Icons.person),
+            ),
+            const CustomInputField(
+              hintText: 'Age',
+              icon: Icon(Icons.numbers),
+            ),
+            const Row(
+              children: [
+                Expanded(
+                  child: CustomInputField(
+                    hintText: 'Height',
+                    icon: Icon(Icons.height),
+                  ),
+                ),
+                Expanded(
+                  child: CustomInputField(
+                    hintText: 'Weight',
+                    icon: Icon(Icons.monitor_weight_outlined),
+                  ),
+                ),
+              ],
+            ),
+            const CustomInputField(
+              hintText: 'Goal',
+              icon: Icon(Icons.height),
+            ),
+            CustomButton(title: 'Register', onPressed: () {}),
+          ],
         ),
       ),
     );
