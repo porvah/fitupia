70d15234edaa0961f19d51d1e3e0305feef65dfc
porvah <3msabdullah@gmail.com>
2023-11-07 00:36:32 +0200

    calendar finished

--- lib/presentation/widgets/calendar.dart
+++ lib/presentation/widgets/calendar.dart
@@ -11,69 +11,35 @@ class Calendar extends StatefulWidget {
 }
 
 class CalendarState extends State<Calendar> {
-  String today = '-';
+  String _today = '';
   bool _openMenu = false;
   void toggleOpenMenu() {
     _openMenu = _openMenu ? false : true;
   }
 
-  final List<List<String>> _days = [
-    ["Saterday", "SA"],
-    ["Sunday", "SU"],
-    ["Monday", "MO"],
-    ["Tuesday", "TU"],
-    ["Wednesday", "WE"],
-    ["Thursday", "TH"],
-    ["Friday", "FR"]
+  final List<int> _schedule = [
+    0,
+    0,
+    0,
+    0,
+    0,
   ];
-  final List<List<String>> _exercises = [
-    ["Chest Day", ""],
-    ["Abs Day", ""],
-    ["Arm Day", ""],
-    ["Back Day", ""],
-    ["Leg Day", ""]
+  final List<String> _days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saterday",
+    "Sunday"
   ];
-  ListView _buildDropDown() {
-    return ListView.builder(
-      itemCount: _exercises.length,
-      itemBuilder: (context, index) {
-        return Container(
-          width: SizeConfig.screenWidth,
-          padding:
-              EdgeInsets.only(top: SizeConfig.getProportionateScreenHeight(20)),
-          child: Row(
-            children: [
-              Container(
-                padding: EdgeInsets.only(
-                    left: SizeConfig.getProportionateScreenWidth(20)),
-                child: Text(
-                  _exercises[index][0],
-                  style: const TextStyle(fontSize: 15),
-                ),
-              ),
-              Container(
-                alignment: Alignment.centerRight,
-                margin: EdgeInsets.only(
-                    left: SizeConfig.getProportionateScreenWidth(100),
-                    right: SizeConfig.getProportionateScreenWidth(20)),
-                child: DropdownMenu(
-                    dropdownMenuEntries: _buildDropDownEntries(),
-                    onSelected: (value) => _exercises[index][1] = value),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  List<DropdownMenuEntry> _buildDropDownEntries() {
-    List<DropdownMenuEntry> entries = [];
-    for (int i = 0; i < _days.length; i++) {
-      entries.add(DropdownMenuEntry(value: _days[i][1], label: _days[i][0]));
-    }
-    return entries;
-  }
+  final List<String> _exercises = [
+    "Chest Day",
+    "Abs Day",
+    "Arm Day",
+    "Back Day",
+    "Leg Day"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -90,60 +56,142 @@ class CalendarState extends State<Calendar> {
                 timeIntervalHeight: 0),
           ),
         ),
-        Container(
-          child: Column(
-            children: [
-              Container(
-                color: const Color(0xFFc1121f),
-                width: SizeConfig.screenWidth,
-                padding:
-                    EdgeInsets.all(SizeConfig.getProportionateScreenHeight(20)),
-                child: Container(
-                  width: SizeConfig.screenWidth,
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(
-                            left: SizeConfig.getProportionateScreenWidth(30)),
-                        child: Text(
-                          "TODAY: $today Day",
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                              fontSize: 20, color: Colors.white),
-                        ),
+        Column(
+          children: [
+            Container(
+              color: const Color(0xFFc1121f),
+              width: SizeConfig.screenWidth,
+              padding:
+                  EdgeInsets.all(SizeConfig.getProportionateScreenHeight(20)),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      margin: EdgeInsets.only(
+                          left: SizeConfig.getProportionateScreenWidth(30)),
+                      child: Text(
+                        "TODAY: $_today",
+                        textAlign: TextAlign.center,
+                        style:
+                            const TextStyle(fontSize: 20, color: Colors.white),
                       ),
-                      Container(
-                        margin: EdgeInsets.only(
-                            left: SizeConfig.getProportionateScreenWidth(100)),
-                        alignment: Alignment.centerRight,
-                        child: FloatingActionButton(
-                          child: _openMenu
-                              ? const Icon(Icons.check)
-                              : const Icon(Icons.edit),
-                          onPressed: () {
-                            setState(() {
-                              if (_openMenu) {
-                                toggleOpenMenu();
-                              } else {
-                                toggleOpenMenu();
-                                today = _exercises[0][1];
-                              }
-                            });
-                          },
-                        ),
-                      )
-                    ],
+                    ),
                   ),
-                ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: FloatingActionButton(
+                      child: _openMenu
+                          ? const Icon(Icons.check)
+                          : const Icon(Icons.edit),
+                      onPressed: () {
+                        setState(() {
+                          if (_openMenu) {
+                            if (_checkSchedule(_schedule)) {
+                              _updateToday();
+                              toggleOpenMenu();
+                            } else {
+                              showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                        title: const Text("Warning!"),
+                                        content: const Text(
+                                          "Please Enter Your Schedule Correctly",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: const Text("ok",
+                                                  style:
+                                                      TextStyle(fontSize: 20)))
+                                        ],
+                                      ));
+                            }
+                          } else {
+                            toggleOpenMenu();
+                          }
+                        });
+                      },
+                    ),
+                  )
+                ],
               ),
-              AnimatedContainer(
-                  duration: const Duration(milliseconds: 200),
-                  height: _openMenu ? 300 : 0,
-                  child: _buildDropDown())
-            ],
-          ),
+            ),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              height: _openMenu ? 300 : 0,
+              child: _buildDropDown(),
+            )
+          ],
         )
       ],
     );
+  }
+
+  ListView _buildDropDown() {
+    return ListView.builder(
+      itemCount: _exercises.length,
+      itemBuilder: (context, index) {
+        return Container(
+          width: SizeConfig.screenWidth,
+          padding:
+              EdgeInsets.only(top: SizeConfig.getProportionateScreenHeight(20)),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  padding: EdgeInsets.only(
+                      left: SizeConfig.getProportionateScreenWidth(20)),
+                  child: Text(
+                    _exercises[index],
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                    right: SizeConfig.getProportionateScreenWidth(20)),
+                child: DropdownMenu(
+                    dropdownMenuEntries: _buildDropDownEntries(),
+                    onSelected: (value) {
+                      _schedule[index] = value;
+                    }),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  List<DropdownMenuEntry> _buildDropDownEntries() {
+    List<DropdownMenuEntry> entries = [];
+    for (int i = 0; i < _days.length; i++) {
+      entries.add(DropdownMenuEntry(value: i + 1, label: _days[i]));
+    }
+    return entries;
+  }
+
+  bool _checkSchedule(List<int> schedule) {
+    List<int> unique = [];
+    for (int element in schedule) {
+      if (!unique.contains(element)) {
+        unique.add(element);
+      }
+    }
+    return (unique.length == schedule.length && !schedule.contains(0));
+  }
+
+  void _updateToday() {
+    int todayWeekday = DateTime.now().weekday;
+    if (_schedule.contains(todayWeekday)) {
+      _today = _exercises[_schedule.indexOf(todayWeekday)];
+    } else {
+      _today = 'Free Day';
+    }
   }
 }
