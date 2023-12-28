063f9b700ca16529feab8b67a8d4e3d999849d47
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-12-29 01:10:59 +0200

    Finishing the design of the search bar. Opening and closing the search bar when the user press on it.

--- lib/presentation/screens/meals_screen.dart
+++ lib/presentation/screens/meals_screen.dart
@@ -5,8 +5,17 @@ import 'package:flutter/material.dart';
 import '../helper/meals.dart';
 import '../widgets/custom_bottom_sheet.dart';
 
-class MealsScreen extends StatelessWidget {
+class MealsScreen extends StatefulWidget {
   static const String routeName = '/meals_screen';
+  final int toBeDisplayedList;
+
+  const MealsScreen(this.toBeDisplayedList, {super.key});
+
+  @override
+  State<MealsScreen> createState() => _MealsScreenState();
+}
+
+class _MealsScreenState extends State<MealsScreen> {
   final pagenameList = const [
     'Dairy product',
     'Meats and beef',
@@ -16,24 +25,29 @@ class MealsScreen extends StatelessWidget {
     'Nuts and seeds',
     'Drinks'
   ];
-  final int toBeDisplayedList;
+  bool isSearching = false;
+  final TextEditingController _controller = TextEditingController();
 
-  const MealsScreen(this.toBeDisplayedList, {super.key});
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(context),
+      appBar: isSearching ? _buildSearchAppBar() : _buildDefaultAppBar(context),
       body: SafeArea(
         child: ListView.builder(
-          itemCount: meals[toBeDisplayedList].length,
+          itemCount: meals[widget.toBeDisplayedList].length,
           itemBuilder: (ctx, index) {
             return MealWidget(
-              mealModel: meals[toBeDisplayedList][index],
+              mealModel: meals[widget.toBeDisplayedList][index],
               buttonTitle: 'Add',
               icon: Icons.add,
-              onPressed: () =>
-                  _openBottomSheet(context, meals[toBeDisplayedList][index]),
+              onPressed: () => _openBottomSheet(
+                  context, meals[widget.toBeDisplayedList][index]),
             );
           },
         ),
@@ -51,22 +65,65 @@ class MealsScreen extends StatelessWidget {
     );
   }
 
-  AppBar _buildAppBar(BuildContext context) {
+  AppBar _buildDefaultAppBar(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.green,
+      foregroundColor: Colors.white,
       title: Text(
-        pagenameList[toBeDisplayedList],
+        pagenameList[widget.toBeDisplayedList],
         style: const TextStyle(
           color: Colors.white,
         ),
       ),
-      foregroundColor: Colors.white,
       actions: [
         IconButton(
-          onPressed: () {},
+          onPressed: _startSearching,
           icon: const Icon(Icons.search),
         ),
       ],
     );
+  }
+
+  AppBar _buildSearchAppBar() {
+    return AppBar(
+      backgroundColor: Colors.green,
+      foregroundColor: Colors.white,
+      title: _buildTextField(),
+    );
+  }
+
+  Widget _buildTextField() {
+    return TextField(
+      controller: _controller,
+      decoration: const InputDecoration(
+        border: InputBorder.none,
+        hintText: 'Search for meal',
+        hintStyle: TextStyle(color: Colors.white54, fontSize: 18),
+      ),
+      cursorColor: Colors.white,
+      autofocus: true,
+      enableSuggestions: true,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  void _startSearching() {
+    setState(() {
+      ModalRoute.of(context)!.addLocalHistoryEntry(
+        LocalHistoryEntry(onRemove: _stopSearching),
+      );
+      isSearching = true;
+    });
+  }
+
+  void _stopSearching() {
+    setState(() {
+      isSearching = false;
+      _controller.clear();
+    });
   }
 }
