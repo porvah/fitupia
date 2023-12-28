41816362caffe54a9d1849de5c1479e108b13010
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-12-29 00:53:47 +0200

    Changing the app bar of meals and add the search icon.

--- lib/presentation/screens/meals_screen.dart
+++ lib/presentation/screens/meals_screen.dart
@@ -1,5 +1,4 @@
 import 'package:first_app/models/meal_model.dart';
-import 'package:first_app/presentation/themes/appbar.dart';
 import 'package:first_app/presentation/widgets/meal_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -17,13 +16,14 @@ class MealsScreen extends StatelessWidget {
     'Nuts and seeds',
     'Drinks'
   ];
-  const MealsScreen(this.toBeDisplayedList, {super.key});
   final int toBeDisplayedList;
+
+  const MealsScreen(this.toBeDisplayedList, {super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(
-          pagenameList[toBeDisplayedList], Colors.white, Colors.green),
+      appBar: _buildAppBar(context),
       body: SafeArea(
         child: ListView.builder(
           itemCount: meals[toBeDisplayedList].length,
@@ -43,10 +43,30 @@ class MealsScreen extends StatelessWidget {
 
   void _openBottomSheet(BuildContext context, MealModel mealModel) {
     showModalBottomSheet(
-        context: context,
-        isScrollControlled: true,
-        builder: (ctx) {
-          return CustomBottomSheet(meal: mealModel);
-        });
+      context: context,
+      isScrollControlled: true,
+      builder: (ctx) {
+        return CustomBottomSheet(meal: mealModel);
+      },
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.green,
+      title: Text(
+        pagenameList[toBeDisplayedList],
+        style: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      foregroundColor: Colors.white,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search),
+        ),
+      ],
+    );
   }
 }
