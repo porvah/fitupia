a01f5e36d3342cc61fee4dec8b722ed0a8ee4b38
porvah <3msabdullah@gmail.com>
2023-12-29 14:27:22 +0200

    reading from json "exercise_of_boddypart_api"

--- lib/models/exercises_of_bodypart_API.dart
+++ lib/models/exercises_of_bodypart_API.dart
@@ -1,24 +1,28 @@
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:http/http.dart';
+import 'package:flutter/services.dart';
 
-class ExercisesOfBodyPartAPI{
+class ExercisesOfBodyPartAPI {
   String bodyPart;
   ExercisesOfBodyPartAPI({required this.bodyPart});
 
-  Future<List<dynamic>> getData() async{
+  Future<List<dynamic>> getData() async {
     List<dynamic> data = [];
-    try{
-      Response response = await get(
-        Uri.parse('https://exercisedb.p.rapidapi.com/exercises/bodyPart/$bodyPart'), 
-        headers: {
-          'X-RapidAPI-Key': 'c123c06545mshc095c405761be37p141cc9jsn4cc33e1eecc4',
-          'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-        }
-      );
-       data = jsonDecode(response.body);
-    }
-    catch(e){
+    try {
+      // Response response = await get(
+      //   Uri.parse('https://exercisedb.p.rapidapi.com/exercises/bodyPart/$bodyPart'),
+      //   headers: {
+      //     'X-RapidAPI-Key': 'c123c06545mshc095c405761be37p141cc9jsn4cc33e1eecc4',
+      //     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      //   }
+      // );
+      var input = await rootBundle
+          .loadString('assets/exercises_data/partExercises.json');
+      var map = jsonDecode(input);
+      var data = map[bodyPart];
+      print(data);
+    } catch (e) {
       print("error => $e");
     }
     return data;
